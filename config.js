64fb6892a5e25aa2a357f9711143c22961ee5511
Drew LeSueur <drewalex@gmail.com>
2013-07-06 01:06:26 +0000

    more sprites

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 var root = (typeof window == "undefined") ? global : window
 
 var viewDimension = 640
-var gridDimension = 10
+var gridDimension = 10 //10 // because my sprites are 8 * 8, this number shold be divisible by 80 (640/8)
 
 root.config = {
   viewWidth: viewDimension,
